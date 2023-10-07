e04d5db6cdce6e796898a5fd43d604aa8009d9a4
Blankeos <carloantonioct@gmail.com>
2023-10-08 00:51:53 +0800

    fix(news): polished styles.

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -29,10 +29,10 @@ const NewsOverviewPage: NextPageWithLayout<
 
   return (
     <div className={container({ class: 'gap-y-10 pt-5 pb-20' })}>
-      <div className="relative">
-        <div className="relative h-80 w-full object-cover overflow-hidden">
+      <Link href={`/news/${featuredNews?.slug}`} className="group relative">
+        <div className="relative h-80 w-full object-cover overflow-hidden rounded-md">
           <Image
-            className="w-full h-full object-cover rounded-2xl"
+            className="w-full h-full object-cover group-hover:scale-105 transition"
             src={featuredNews?.featureImage()?.url ?? ''}
             fill
             alt={featuredNews?.featureImage()?.alt ?? ''}
@@ -41,20 +41,24 @@ const NewsOverviewPage: NextPageWithLayout<
         <div className="flex justify-center">
           <div
             ref={featuredOverlayRef}
-            className="w-11/12 p-5 absolute bg-[#F9F9FF] bottom-0"
+            className="w-11/12 p-5 absolute bg-[#F9F9FF] bottom-0 group-hover:bg-primary-500 transition"
           >
-            <h2 className="text-dark-500 text-2xl font-bold">
+            <h2 className="text-dark-500 text-2xl font-bold group-hover:text-white transition">
               {featuredNews?.title}
             </h2>
-            <p className="text-dark-400">
+            <p className="text-dark-400 group-hover:text-primary-200 transition">
               {extractTextFromContent(featuredNews?.content())
                 .join(' ')
-                .slice(0, 250)}
+                .slice(0, 250)
+                .trim()}
               ...
             </p>
             <div className="h-8" />
-            <div className="text-primary-300 font-bold text-sm">
-              {formatDate(featuredNews?.publishedDate)}
+            <div className="text-primary-300 font-bold text-sm flex justify-between">
+              <span>{formatDate(featuredNews?.publishedDate)}</span>
+              <span className="group-hover:opacity-100 group-hover:translate-y-0 translate-y-5 opacity-0 transition">
+                Read More
+              </span>
             </div>
           </div>
         </div>
@@ -64,7 +68,7 @@ const NewsOverviewPage: NextPageWithLayout<
             height: (featuredOverlaySize?.height ?? 2) / 2,
           }}
         />
-      </div>
+      </Link>
 
       <h2 className="text-3xl text-primary-500 font-bold">Latest Happenings</h2>
 
@@ -113,12 +117,12 @@ const NewsCard: FC<NewsCardType> = (props) => {
 
   return (
     <Link href={href} className="flex flex-col gap-y-4 group">
-      <div className="relative aspect-[5/4] overflow-hidden rounded-md group-hover:scale-[.98] transition bg-primary-50">
+      <div className="relative aspect-[5/4] overflow-hidden rounded-md transition bg-primary-50">
         <Image
           src={image.url}
           fill
           alt={image.alt ?? title}
-          className="object-cover"
+          className="object-cover group-hover:scale-105 transition"
         />
       </div>
       <h3 className="text-dark-700 font-medium line-clamp-4">{title}</h3>
