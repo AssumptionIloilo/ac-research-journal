1a790e4e5e55b63b4df9a017883b073cb09cd868
Blankeos <carloantonioct@gmail.com>
2023-11-02 00:10:35 +0800

    chore: test api for post.

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -8,5 +8,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' });
+  //   console.log(req.method);
+  if (req.method === 'POST') {
+    res.status(200).json({ message: 'A POST from Next.js!' });
+    return;
+  }
+
+  res.status(200).json({ message: 'Hellos from Next.js!' });
 }
